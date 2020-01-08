4f8ee38e7f877cac4481e7c2921acbc3fef0637f
rodrigo-orellana <rodrigoesteban@correo.ugr.es>
2020-01-08 19:27:50 +0100

    mejores practicas disposicion codigo
    Closes #57

---
+++ test/test_desafio.py
@@ -0,0 +1,25 @@
+import sys
+sys.path.append("../")
+
+import unittest
+from desafio import Desafio
+
+
+class TestModel(unittest.TestCase):
+
+    def setUp(self):
+        self.prueba1 = Desafio("Reciclaje", '2019-06-26',
+                               '2020-06-26', "Chile", "Santiago")
+        self.prueba2 = Desafio("Reciclaje", '2019-06-26',
+                               '2020-06-26', "Chile", "Santiago")
+
+    def testTipoCreacion(self):
+        self.assertIsInstance(self.prueba1, Desafio,
+                              "Tipo de objeto jugador incorrecto.")
+
+    def testUnicidad(self):
+        self.assertIsNot(self.prueba1, self.prueba2,
+                         "Dos objetos con los mismos atributos no pueden ser el mismo.")
+
+if __name__ == '__main__':
+    unittest.main()
