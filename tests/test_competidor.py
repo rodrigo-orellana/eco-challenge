379a2ed89607e89d893ce1f48368f031c97ac274
rodrigo-orellana <rodrigoesteban@correo.ugr.es>
2020-01-09 23:38:37 +0100

    single source of truth
    Closes #58

--- tests/test_competidor.py
+++ tests/test_competidor.py
@@ -1,25 +1,42 @@
 import unittest
 import sys
+import random
 sys.path.append('challenge')
 
 from competidor import Competidor
+from mongoDB import BaseDatos
+import os
+import logging
 
+user = os.environ.get("USER_MBD")
+passw = os.environ.get("PASS_MBD")
+ambiente = os.environ.get("AMBIENTE")
+if ambiente != "localhost":
+    logging.info("base de datos cloud"+str(ambiente))
+    mongo = BaseDatos(
+        "mongodb+srv://"+str(user)+":"+str(passw)+"@cluster0-qazzt.mongodb.net/desafio?retryWrites=true&w=majority", False)
+else:
+    logging.info("base de datos local")
+    mongo = BaseDatos("mongodb://127.0.0.1:27017/MiBaseDatos", True)
+
+# inyeccion de dependencia
+competidor_data = Competidor(data_access=mongo)
 
 class TestModel(unittest.TestCase):
 
     def setUp(self):
-        self.prueba1 = Competidor("Sebastian Piñera", '2019-06-26',
+        self.prueba1 = competidor_data.create("Sebastian Test1"+str(random.uniform(1, 1000)), '2019-06-26',
                                "60","Masculino" , "Chile", "Santiago")
-        self.prueba2 = Competidor("Juan Carlos", '2019-06-26',
+        self.prueba2 = competidor_data.create("Juan Test2"+str(random.uniform(1, 1000)), '2019-06-26',
                                "70","Masculino" , "España", "Madrid")
 
     def testTipoCreacion(self):
-        self.assertIsInstance(self.prueba1, Competidor,
-                              "Tipo de objeto jugador incorrecto.")
+        self.assertIsNotNone(self.prueba1,
+                              "Tipo de objeto competidor incorrecto.")
+    def testTipoCreacion2(self):
+        self.assertIsNotNone(self.prueba2,
+                              "Tipo de objeto competidor incorrecto.")
 
-    def testUnicidad(self):
-        self.assertIsNot(self.prueba1, self.prueba2,
-                         "Dos objetos con los mismos atributos no pueden ser el mismo.")
 
 if __name__ == '__main__':
     unittest.main()
--- tests/test_desafio.py
+++ tests/test_desafio.py
@@ -1,24 +1,42 @@
 import unittest
 import sys
+import random
 sys.path.append('challenge')
-from desafio import Desafio
 
+from desafio import Desafio
+from mongoDB import BaseDatos
+import os
+import logging
+
+user = os.environ.get("USER_MBD")
+passw = os.environ.get("PASS_MBD")
+ambiente = os.environ.get("AMBIENTE")
+if ambiente != "localhost":
+    logging.info("base de datos cloud"+str(ambiente))
+    mongo = BaseDatos(
+        "mongodb+srv://"+str(user)+":"+str(passw)+"@cluster0-qazzt.mongodb.net/desafio?retryWrites=true&w=majority", False)
+else:
+    logging.info("base de datos local")
+    mongo = BaseDatos("mongodb://127.0.0.1:27017/MiBaseDatos", True)
+
+# inyeccion de dependencia
+desafio_data = Desafio(data_access=mongo)
 
 class TestModel(unittest.TestCase):
 
     def setUp(self):
-        self.prueba1 = Desafio("Reciclaje", '2019-06-26',
+        self.prueba1 = desafio_data.create("Reciclaje"+str(random.uniform(1, 1000)), '2019-06-26',
                                '2020-06-26', "Chile", "Santiago")
-        self.prueba2 = Desafio("Reciclaje", '2019-06-26',
-                               '2020-06-26', "Chile", "Santiago")
+        self.prueba2 = desafio_data.create("Reciclaje2"+str(random.uniform(1, 1000)), '2020-06-26',
+                               '2021-06-26', "Chile", "Santiago")
 
     def testTipoCreacion(self):
-        self.assertIsInstance(self.prueba1, Desafio,
-                              "Tipo de objeto jugador incorrecto.")
+        self.assertIsNotNone(self.prueba1,
+                              "Tipo de objeto desafio incorrecto.")
+    def testTipoCreacion2(self):
+        self.assertIsNotNone(self.prueba2,
+                              "Tipo de objeto desafio incorrecto.")
 
-    def testUnicidad(self):
-        self.assertIsNot(self.prueba1, self.prueba2,
-                         "Dos objetos con los mismos atributos no pueden ser el mismo.")
 
 if __name__ == '__main__':
     unittest.main()
