74714b0c810f260f1b87111bce336b975201d2a1
rodrigo-orellana <rodrigoesteban@correo.ugr.es>
2020-01-10 02:21:21 +0100

    single source of truth
    Closes #58

--- tests/test_competidor.py
+++ tests/test_competidor.py
@@ -1,42 +1,70 @@
 import unittest
 import sys
-import random
 sys.path.append('challenge')
-
+from unittest.mock import MagicMock
 from competidor import Competidor
-from mongoDB import BaseDatos
+
 import os
 import logging
 
-user = os.environ.get("USER_MBD")
-passw = os.environ.get("PASS_MBD")
-ambiente = os.environ.get("AMBIENTE")
-if ambiente != "localhost":
-    logging.info("base de datos cloud"+str(ambiente))
-    mongo = BaseDatos(
-        "mongodb+srv://"+str(user)+":"+str(passw)+"@cluster0-qazzt.mongodb.net/desafio?retryWrites=true&w=majority", False, "competidor")
-else:
-    logging.info("base de datos local")
-    mongo = BaseDatos("mongodb://127.0.0.1:27017/MiBaseDatos", True, "competidor")
-
-# inyeccion de dependencia
-competidor_data = Competidor(data_access=mongo)
 
 class TestModel(unittest.TestCase):
 
     def setUp(self):
-        self.prueba1 = competidor_data.create("Sebastian Test1"+str(random.uniform(1, 1000)), '2019-06-26',
-                               "60","Masculino" , "Chile", "Santiago")
-        self.prueba2 = competidor_data.create("Juan Test2"+str(random.uniform(1, 1000)), '2019-06-26',
-                               "70","Masculino" , "España", "Madrid")
+        #  uso de MOCK para simular conexion BD
+        self.mock = MagicMock()
+        # crea instancia
+        self.competidor = Competidor(data_access=self.mock)
 
-    def testTipoCreacion(self):
-        self.assertIsNotNone(self.prueba1,
-                              "Tipo de objeto competidor incorrecto.")
-    def testTipoCreacion2(self):
-        self.assertIsNotNone(self.prueba2,
-                              "Tipo de objeto competidor incorrecto.")
+        # asigna datos:
+        self.sample_competidor = dict(
+            nombre="Juan_test0001",
+            fecha_ins='2019-06-26',
+            edad="38",
+            sexo="MASCULINO",
+            pais="Chile",
+            ciudad="Santiago" 
+        )
 
+        # Create a sample id:
+        self.sample_id = '000000000000000000000000'
+  
+    def test_create_ok(self):
+        """ Test if a new competidor is inserted on list """
+        # Configure the mock for returning an id on insert, and None on get:
+        self.mock.insert.return_value = self.sample_id
+        self.mock.get.return_value = None
+
+        id = self.competidor.create(
+            self.sample_competidor["nombre"], self.sample_competidor["fecha_ins"], 
+            self.sample_competidor["edad"], self.sample_competidor["sexo"], 
+            self.sample_competidor["pais"], self.sample_competidor["ciudad"]
+        )
+
+        # Ensure that mock had been called with correct arguments:
+        self.mock.insert.assert_called_with(self.sample_competidor)
+        self.mock.get.assert_called_with(key='nombre', value=self.sample_competidor['nombre'])
+        self.assertEqual(id, self.sample_id)
+
+        
+    def test_search_by_name(self):
+        """ Test the search for existing and non-existing competidor by name """
+        # Existing competidor:
+        self.sample_competidor['_id'] = self.sample_id
+        self.mock.get.return_value = self.sample_competidor
+
+        res_found = self.competidor.search_by_name(self.sample_competidor["nombre"])
+        self.assertEqual(res_found, self.sample_competidor)
+        self.mock.get.assert_called_with(key='nombre', value=self.sample_competidor['nombre'])
+
+        # Non-existing competidor:
+        self.mock.get.return_value = None        
+        
+        res_not_found = self.competidor.search_by_name("Non-existing competidor")
+        self.assertEqual(res_not_found, None)
+        self.mock.get.assert_called_with(key='nombre', value="Non-existing competidor")
 
 if __name__ == '__main__':
     unittest.main()
+
+     
--- tests/test_desafio.py
+++ tests/test_desafio.py
@@ -1,42 +1,67 @@
 import unittest
 import sys
-import random
 sys.path.append('challenge')
-
+from unittest.mock import MagicMock
 from desafio import Desafio
-from mongoDB import BaseDatos
+
 import os
 import logging
 
-user = os.environ.get("USER_MBD")
-passw = os.environ.get("PASS_MBD")
-ambiente = os.environ.get("AMBIENTE")
-if ambiente != "localhost":
-    logging.info("base de datos cloud"+str(ambiente))
-    mongo = BaseDatos(
-        "mongodb+srv://"+str(user)+":"+str(passw)+"@cluster0-qazzt.mongodb.net/desafio?retryWrites=true&w=majority", False, "desafio")
-else:
-    logging.info("base de datos local")
-    mongo = BaseDatos("mongodb://127.0.0.1:27017/MiBaseDatos", True, "desafio")
-
-# inyeccion de dependencia
-desafio_data = Desafio(data_access=mongo)
 
 class TestModel(unittest.TestCase):
 
     def setUp(self):
-        self.prueba1 = desafio_data.create("Reciclaje"+str(random.uniform(1, 1000)), '2019-06-26',
-                               '2020-06-26', "Chile", "Santiago")
-        self.prueba2 = desafio_data.create("Reciclaje2"+str(random.uniform(1, 1000)), '2020-06-26',
-                               '2021-06-26', "Chile", "Santiago")
+        #  uso de MOCK para simular conexion BD
+        self.mock = MagicMock()
+        # crea instancia
+        self.desafio = Desafio(data_access=self.mock)
 
-    def testTipoCreacion(self):
-        self.assertIsNotNone(self.prueba1,
-                              "Tipo de objeto desafio incorrecto.")
-    def testTipoCreacion2(self):
-        self.assertIsNotNone(self.prueba2,
-                              "Tipo de objeto desafio incorrecto.")
+        # asigna datos:
+        self.sample_desafio = dict(
+            nombre="Reciclaje_test0001",
+            fecha_ini='2019-06-26',
+            fecha_fin='2019-06-26',
+            pais="Chile",
+            ciudad="Santiago", 
+        )
 
+        # Create a sample id:
+        self.sample_id = '000000000000000000000000'
+  
+    def test_create_ok(self):
+        """ Test if a new desafio is inserted on list """
+        # Configure the mock for returning an id on insert, and None on get:
+        self.mock.insert.return_value = self.sample_id
+        self.mock.get.return_value = None
+
+        id = self.desafio.create(
+            self.sample_desafio["nombre"], self.sample_desafio["fecha_ini"], 
+            self.sample_desafio["fecha_fin"], self.sample_desafio["pais"], 
+            self.sample_desafio["ciudad"]
+        )
+
+        # Ensure that mock had been called with correct arguments:
+        self.mock.insert.assert_called_with(self.sample_desafio)
+        self.mock.get.assert_called_with(key='nombre', value=self.sample_desafio['nombre'])
+        self.assertEqual(id, self.sample_id)
+
+        
+    def test_search_by_name(self):
+        """ Test the search for existing and non-existing desafio by name """
+        # Existing desafio:
+        self.sample_desafio['_id'] = self.sample_id
+        self.mock.get.return_value = self.sample_desafio
+
+        res_found = self.desafio.search_by_name(self.sample_desafio["nombre"])
+        self.assertEqual(res_found, self.sample_desafio)
+        self.mock.get.assert_called_with(key='nombre', value=self.sample_desafio['nombre'])
+
+        # Non-existing desafio:
+        self.mock.get.return_value = None        
+        
+        res_not_found = self.desafio.search_by_name("Non-existing desafio")
+        self.assertEqual(res_not_found, None)
+        self.mock.get.assert_called_with(key='nombre', value="Non-existing desafio")
 
 if __name__ == '__main__':
     unittest.main()
